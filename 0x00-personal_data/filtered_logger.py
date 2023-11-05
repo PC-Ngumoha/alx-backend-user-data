cad6e05201b42ade1418a9648ec574c6eaccc7d1
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-05 03:51:12 +0100

    Update: Implemented RedactingFormatter subclass

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,26 @@ Contains the definition of the function 'filter_datum' which helps
 to filter the data passed into it.
 """
 from typing import List
+import logging
 import re
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return logging.Formatter(self.FORMAT, None, '%').format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
