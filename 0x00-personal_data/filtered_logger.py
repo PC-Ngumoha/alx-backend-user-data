2c92d986a28cb1d64d3bd235e66e56645b3f49a8
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-05 03:56:47 +0100

    Update: Added some more documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,10 +19,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]) -> None:
+        """ __init__() method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ format() method """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return logging.Formatter(self.FORMAT, None, '%').format(record)
