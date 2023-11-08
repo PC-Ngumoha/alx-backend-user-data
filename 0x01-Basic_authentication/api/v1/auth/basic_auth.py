f722ac72dc66c74d5f7ffb0a968d24966499a51a
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-08 03:21:31 +0100

    Update: Added a new procedure to the BasicAuth authentication class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,15 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """class BasicAuth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract_base64_authorization_header() method
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        else:
+            return authorization_header.replace('Basic ', '')
