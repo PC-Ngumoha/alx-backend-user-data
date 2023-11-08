c30e5a74abd087f37397c98bcb2e482080d0180e
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-08 05:25:48 +0100

    Update: Completed M.V.P of basic_auth app

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -75,3 +75,13 @@ class BasicAuth(Auth):
         if not user.is_valid_password(user_pwd):
             return None
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user() method
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        email, password = self.extract_user_credentials(decoded_header)
+        user = self.user_object_from_credentials(email, password)
+        return user
