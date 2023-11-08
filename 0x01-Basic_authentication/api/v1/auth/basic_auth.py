43b9b30777ac7c3c840ab5f49567ae9e4ca9766c
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-08 03:35:00 +0100

    Update: Adds the ability to decode base 64 strings

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Contains the BasicAuth class which handles authentication through the use
 of the Basic Authentication mechanism for RESTful APIs
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,19 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.replace('Basic ', '')
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode_base64_authorization_header() method
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        else:
+            try:
+                output = base64.b64decode(base64_authorization_header)
+            except binascii.Error:
+                return None
+            return output.decode('utf-8')
