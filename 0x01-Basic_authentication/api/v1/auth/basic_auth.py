ccda743f4498c853b5d67652547565f65a835984
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-08 03:04:14 +0100

    Update: Created a BasicAuth class to encapsulate the mechanism for Basic Authentication of the RESTful API

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+api/v1/auth/basic_auth.py
+Contains the BasicAuth class which handles authentication through the use
+of the Basic Authentication mechanism for RESTful APIs
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """class BasicAuth
+    """
+    pass
