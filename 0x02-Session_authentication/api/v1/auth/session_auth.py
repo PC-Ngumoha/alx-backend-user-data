186816e71f1cdf3aa8869f23d9937da572c55a26
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-12 21:12:56 +0100

    Update: Setup SessionAuth class dor setting up session authentication capabilities in the project.

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+/api/v1/auth/session_auth.py
+Contains the SessionAuth class which handles authentication through the
+use of Session Authentication mechanisms for RESTful APIs.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class SessionAuth
+    """
+    pass
