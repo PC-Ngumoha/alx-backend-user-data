0afe97c2859ed9c4de427af2210977d134eb465d
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-15 22:54:38 +0100

    Update: Implement Auth method: 'create_session' which creates a new session for the user and returns it's ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,20 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Generates a new session ID for the user in DB with given email.
+
+        Parameters:
+          - email: email address provided
+
+        Returns:
+          - UUID string representing the session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            pass
