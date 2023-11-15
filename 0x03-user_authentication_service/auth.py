4d8510e4e51187ba003d0ec0fd84a1c8f2c430ca
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-15 21:26:03 +0100

    Update: Implement the Auth method:

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Auth module
 """
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 import bcrypt
 
 
@@ -14,3 +17,30 @@ def _hash_password(password: str) -> bytes:
       - hashed: Hash generated from password + salt
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user on the DB
+
+        Parameters:
+          - email: unique email address of the new user
+          - password: user password
+
+        Returns:
+          - new_user: the new user created
+        """
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            pass
+        new_user = self._db.add_user(email, _hash_password(password))
+        return new_user
