3bc787ccae084e0cd7dd339f95fb9fec0f71aa4a
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-15 21:53:56 +0100

    Update: Implement new route for registering new users into the app using the API

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """App Module: Flask App
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
@@ -12,6 +14,22 @@ def root_route_handler() -> str:
     """
     return jsonify({'message': 'Bienvenue'})
 
+
+@app.route('/users', strict_slashes=False, methods=['POST'])
+def users() -> str:
+    """Registers a new user on the app.
+    """
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            AUTH.register_user(email, password)
+        except ValueError:
+            return jsonify({'message': 'email already registered'}), 400
+        return jsonify({
+            'email': '{}'.format(email),
+            'message': 'user created'
+        })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
