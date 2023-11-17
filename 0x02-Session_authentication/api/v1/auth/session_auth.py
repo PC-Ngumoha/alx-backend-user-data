4425ba18f7cfee98f894c94e063cb8d7b627d2ee
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-17 15:09:23 +0100

    Update: Create SessionAuth method which enables retrieval of user_id using session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session for the user with ID: user_id
+        """Creates & stores a session_id for the user with ID: user_id
 
         Parameters:
             - user_id: ID of the user we wish to be able to identify.
@@ -29,3 +29,18 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user_id associated with the provided session_id
+
+        Parameters:
+            - session_id: ID for the session linked to user_id
+
+        Returns:
+            - user_id: ID of the user linked to this session_id
+        """
+        if session_id is None:
+            return
+        if type(session_id) is not str:
+            return
+        return self.user_id_by_session_id.get(session_id)
